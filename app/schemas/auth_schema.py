262e3e2666c9eb7b9245b5cf4bd6750b90a2eb90
Rafael Veríssimo da Silva (Dev. SRE Seller) <rafael.verissimo@luizalabs.com>
2025-06-04 20:50:58 -0300

    subindo backend

---
+++ app/schemas/auth_schema.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+class UserBase(BaseModel):
+    name: str
+    email: EmailStr
+
+class UserCreate(UserBase):
+    password: str  # Senha necessária para registro
+
+class UserResponse(UserBase):
+    id: int
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenPayload(BaseModel):
+    sub: EmailStr
+    id: int
+    name: str

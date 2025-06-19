1c8871a32b8563e73749ff71963fcd306bdc03f8
EduardoVBF <eduardo.freitas15@fatec.sp.gov.br>
2025-06-19 12:13:24 -0300

    new base url - VM azure

--- mobileFront/utils/api.js
+++ mobileFront/utils/api.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = "https://hoop-vision.onrender.com";
+// export const API_BASE_URL = "https://hoop-vision.onrender.com";
+export const API_BASE_URL = "http://hoopvision.eastus2.cloudapp.azure.com:8000";
 
 export function apiEndpoint(path) {
   return `${API_BASE_URL}${path}`;
